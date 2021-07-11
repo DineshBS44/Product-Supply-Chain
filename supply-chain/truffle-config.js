93a37bdb77e2d118fbc8b5668471146757c37dfd
Dinesh B S <dineshbs444@gmail.com>
2021-07-11 20:43:56 +0530

    Testing done for all the functions in smart contracts

--- supply-chain/truffle-config.js
+++ supply-chain/truffle-config.js
@@ -27,7 +27,7 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    timeout: 400000,
   },
 
   // Configure your compilers
